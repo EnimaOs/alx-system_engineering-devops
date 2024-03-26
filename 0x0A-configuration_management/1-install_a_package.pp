a9fa2b52509b51d77877b0eaf5046d7f148e5bd7
EnimaOs <ing.devov@gmail.com>
2024-03-26 23:51:41 +0000

    modified

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,28 @@
-#!/usr/bin/pup
-
 # install_flask.pp
+
+# Include the stdlib module for package management
+include stdlib
+
+# Define a class for installing Python and pip3
+class { 'python':
+  version => '3.8.10',
+}
+
 package { 'python3-pip':
-  ensure => installed,
+  ensure   => 'present',
+  provider => 'pip3',
+  require  => Class['python'],
 }
 
 # Install Flask version 2.1.0 and Werkzeug version 2.0.2 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
